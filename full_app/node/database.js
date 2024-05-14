a40cfabc41b43db208af299473a3f289a22ec11f
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-05-14 19:03:07 +0530

    Delete full_app directory

--- full_app/node/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const db = async()=>{
-    await mongoose.connect("mongodb://192.168.29.85:27017/test");
-    console.log("database connected");
-}
-
-module.exports = db;
